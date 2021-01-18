cef0ea19c9f116fcdfe9605a5e71160955ff9c17
Utpal Maiti <Utpalmaiti12@gmail.com>
2021-01-18 14:21:24 +0530

    Initial Commit

---
+++ Special Elements/Special Elements/01. Textbox/EntryPoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+class EntryPoint
+{
+
+    static void Main()
+    {
+
+        IWebDriver driver = new ChromeDriver();
+        IWebElement textBox;
+
+        string url = "http://testing.todorvachev.com/special-elements/text-input-field/";
+        driver.Navigate().GoToUrl(url);
+        textBox = driver.FindElement(By.Name("username"));
+        textBox.SendKeys("Test text");
+        Thread.Sleep(3000);
+        Console.WriteLine(textBox.GetAttribute("maxlength"));
+        Thread.Sleep(3000);
+        driver.Quit();
+    }
+
+}
